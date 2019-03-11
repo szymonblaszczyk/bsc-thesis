2e6ea3d54c53b44892ffeacc56dc03433838ab56
Szymon Błaszczyk <szymon.a.blaszczyk@gmail.com>
2019-03-11 18:07:19 +0100

    Add Eureka discovery service

---
+++ discovery-service/src/test/kotlin/pl/ife/tcs/discoveryservice/DiscoveryServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package pl.ife.tcs.discoveryservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
