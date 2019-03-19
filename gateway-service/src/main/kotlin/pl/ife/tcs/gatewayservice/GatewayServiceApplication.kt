42ee3fd173a699972c2bf7c631dab46beab63333
Szymon Błaszczyk <szymon.a.blaszczyk@gmail.com>
2019-03-19 23:58:38 +0100

    Introduce Gateway service stump

---
+++ gateway-service/src/main/kotlin/pl/ife/tcs/gatewayservice/GatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package pl.ife.tcs.gatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayServiceApplication>(*args)
+}
